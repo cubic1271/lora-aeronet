b8ac172b8bf1c838dc414a025318511b8319b01d
Gilbert Clark <gc355804@ohio.edu>
2018-07-25 23:34:04 -0400

    Initial commit - code to send packets included based closely on example code.  Receive code has been removed for the moment.

---
+++ src/main.cpp
@@ -0,0 +1,162 @@
+#include <Wire.h>  // Only needed for Arduino 1.6.5 and earlier
+#include "SSD1306.h" // alias for `#include "SSD1306Wire.h"`
+
+#include <SPI.h>
+#include <LoRa.h>
+// #include "SSD1306.h"
+
+SSD1306 display(0x3c, 4, 15);
+
+//OLED pins to ESP32 GPIOs via this connection:
+//OLED_SDA — GPIO4
+//OLED_SCL — GPIO15
+//OLED_RST — GPIO16
+
+#define REG_FRF_MSB              0x06
+#define REG_FRF_MID              0x07
+#define REG_FRF_LSB              0x08
+
+// WIFI_LoRa_32 ports
+
+// GPIO5 — SX1278’s SCK
+// GPIO19 — SX1278’s MISO
+// GPIO27 — SX1278’s MOSI
+// GPIO18 — SX1278’s CS
+// GPIO14 — SX1278’s RESET
+// GPIO26 — SX1278’s IRQ(Interrupt Request)
+
+#define SS 18
+#define RST 14
+#define DI0 26
+// #define BAND 429E6
+
+// LoRa Settings
+// #define BAND 434500000.00
+#define BAND 915000000.00
+#define spreadingFactor 8
+#define SignalBandwidth 125000
+// #define SignalBandwidth 31.25E3
+
+#define codingRateDenominator 8
+
+// ADC? Battery voltage
+// const uint8_t vbatPin = 34;
+// float VBAT;  // battery voltage from ESP32 ADC read
+
+int ctr = 0;
+char sbuf[512];
+
+void setup() {
+  uint64_t freq_res;
+  uint8_t tmp_freq;
+  pinMode(16,OUTPUT);
+  digitalWrite(16, LOW); // set GPIO16 low to reset OLED
+  delay(50);
+  digitalWrite(16, HIGH);
+
+  display.init();
+  display.flipScreenVertically();
+  display.setFont(ArialMT_Plain_10);
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+
+  Serial.begin(115200);
+  while (!Serial); //if just the the basic function, must connect to a computer
+  delay(1000);
+
+  Serial.println("LoRa Receiver");
+  display.drawString(5,5,"LoRa Receiver");
+  display.display();
+  SPI.begin(5,19,27,18);
+  LoRa.setPins(SS,RST,DI0);
+
+/*
+  pinMode(vbatPin, INPUT);
+  VBAT = (120.0/20.0) * (float)(analogRead(vbatPin)) / 1024.0; // LiPo battery voltage in volts
+  Serial.println("Vbat = "); Serial.print(VBAT); Serial.println(" Volts");
+*/
+
+  if (!LoRa.begin(BAND, true)) {
+    display.drawString(5,25,"Starting LoRa failed!");
+    while (1);
+  }
+  Serial.println("LoRa Initial OK!");
+
+  Serial.print("LoRa Frequency: ");
+  Serial.println(BAND);
+
+  Serial.print("LoRa Spreading Factor: ");
+  Serial.println(spreadingFactor);
+  LoRa.setSpreadingFactor(spreadingFactor);
+
+  Serial.print("LoRa Signal Bandwidth: ");
+  Serial.println(SignalBandwidth);
+  LoRa.setSignalBandwidth(SignalBandwidth);
+
+  LoRa.setCodingRate4(codingRateDenominator);
+
+  display.drawString(5,25,"Startup completed ...");
+  display.display();
+
+  freq_res = 0;
+  tmp_freq = LoRa.readRegister(REG_FRF_MSB);
+  freq_res |= (tmp_freq << 16);
+  tmp_freq = LoRa.readRegister(REG_FRF_MID);
+  freq_res |= (tmp_freq << 8);
+  tmp_freq = LoRa.readRegister(REG_FRF_LSB);
+  freq_res |= tmp_freq;
+
+  freq_res = freq_res * 32000000;
+  freq_res = freq_res >> 19;
+
+  display.clear();
+  display.setFont(ArialMT_Plain_10);
+  snprintf(sbuf, 512, "Frequency: %u MHz", (uint32_t) freq_res / 1000000);
+  display.drawString(5, 0, sbuf);
+  display.display();
+}
+
+void loop() {
+  display.clear();
+  display.drawString(5,25,"Sending packet ...");
+
+  uint8_t tmp_freq = 0;
+  uint64_t freq_res = 0;
+
+  tmp_freq = LoRa.readRegister(REG_FRF_MSB);
+  freq_res |= (tmp_freq << 16);
+  tmp_freq = LoRa.readRegister(REG_FRF_MID);
+  freq_res |= (tmp_freq << 8);
+  tmp_freq = LoRa.readRegister(REG_FRF_LSB);
+  freq_res |= tmp_freq;
+
+  freq_res = freq_res * 32000000;
+  freq_res = freq_res >> 19;
+
+  // try to parse packet
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    display.clear();
+    snprintf(sbuf, 512, "Frequency: %u MHz", (uint32_t) freq_res / 1000000);
+    display.drawString(5, 0, sbuf);
+    display.display();
+
+    // read packet
+    while (LoRa.available()) {
+      String data = LoRa.readString();
+      display.drawString(20,22, data);
+      display.display();
+    }
+
+    // print RSSI of packet
+    Serial.print(" with RSSI ");
+    Serial.println(LoRa.packetRssi());
+    Serial.print(" with SNR ");
+    Serial.println(LoRa.packetSnr());
+    // display.drawString(0, 45, "RSSI: ");
+    // display.drawString(50, 45, (String)LoRa.packetRssi());
+
+    display.drawString(0, 45, (String)LoRa.packetRssi() + "dB (" + (String)LoRa.packetSnr() +"dB)");
+
+    display.display();
+  }
+}
